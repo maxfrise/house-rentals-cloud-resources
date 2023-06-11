2b962d7f7c7259517dfb1c975cd9d864dd170099
sergio ortiz <audel91@gmail.com>
2023-06-10 20:16:22 -0700

    remove undeclared reference

--- modules/sayhi/outputs.tf
+++ modules/sayhi/outputs.tf
@@ -7,8 +7,3 @@ output "name" {
   description = "Name (id) of the bucket"
   value       = aws_s3_bucket.s3_bucket.id
 }
-
-output "domain" {
-  description = "Domain name of the bucket"
-  value       = aws_s3_bucket_website_configuration.s3_bucket.website_domain
-}
