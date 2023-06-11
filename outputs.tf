6e202df46de7fd8036425a6c4ec30fbaf148e0b7
sergio ortiz <audel91@gmail.com>
2023-06-10 20:18:28 -0700

    remove domain

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "sayhi_bucket_name" {
   description = "Name (id) of the bucket"
   value       = module.sayHi_bucket.name
 }
-
-output "sayhi_bucket_domain" {
-  description = "Domain name of the bucket"
-  value       = module.sayHi_bucket.domain
-}
