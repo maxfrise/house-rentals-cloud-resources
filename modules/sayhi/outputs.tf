a5a29746971cffac7023704e67ac56a3ffddd57b
sergio ortiz <audel91@gmail.com>
2023-06-10 20:22:20 -0700

    fix resource name

--- modules/sayhi/outputs.tf
+++ modules/sayhi/outputs.tf
@@ -1,9 +1,9 @@
 output "arn" {
   description = "ARN of the bucket"
-  value       = aws_s3_bucket.s3_bucket.arn
+  value       = aws_s3_bucket.lambda_bucket.arn
 }
 
 output "name" {
   description = "Name (id) of the bucket"
-  value       = aws_s3_bucket.s3_bucket.id
+  value       = aws_s3_bucket.lambda_bucket.id
 }
