db3ef8ccc28030ae761ea558e83a783552ce24c6
sergio ortiz <audel91@gmail.com>
2023-06-12 19:51:16 -0700

    update-code

--- modules/lambdas3example/src/index.ts
+++ modules/lambdas3example/src/index.ts
@@ -3,6 +3,7 @@ import { Event } from './event';
 
 export const handler: Handler = async (event: Event) => {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+    console.log('new log added')
     return {
         statusCode: 200,
         body: JSON.stringify({
