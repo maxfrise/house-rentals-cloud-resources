acfdb96ac86d515e1c60adbce38444b30479412e
sergio ortiz <audel91@gmail.com>
2023-06-12 20:12:20 -0700

    update sourcecode

--- modules/lambdaexample/main.tf
+++ modules/lambdaexample/main.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "hello-terraform" {
   architectures    = ["arm64"]
   role             = aws_iam_role.iam_for_lambda.arn
   timeout          = 30
-  source_code_hash = base64sha256(path.module + var.lambda_code_filename)
+  source_code_hash =  "${filebase64sha256(path.module + "/${var.lambda_code_filename}")}"
   depends_on = [
     null_resource.build_lambda_function
   ]
