a10dfe59dd80b209222ed22820ec27ad5165c64f
sergio ortiz <audel91@gmail.com>
2023-06-12 20:18:31 -0700

    remove depends on

--- modules/lambdaexample/main.tf
+++ modules/lambdaexample/main.tf
@@ -7,9 +7,6 @@ resource "aws_lambda_function" "hello-terraform" {
   role             = aws_iam_role.iam_for_lambda.arn
   timeout          = 30
   source_code_hash =  filebase64sha256(var.lambda_code_filename)
-  depends_on = [
-    null_resource.build_lambda_function
-  ]
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
