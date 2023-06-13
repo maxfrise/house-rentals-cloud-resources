bf434e46389fbf6e5a07dc843f2c5007ee83927e
sergio ortiz <audel91@gmail.com>
2023-06-12 20:41:33 -0700

    add dynamodb table

---
+++ modules/dynamodbtableexample/main.tf
@@ -0,0 +1,32 @@
+resource "aws_dynamodb_table" "tf_example_table" {
+  name         = "tf-test-table"
+  billing_mode = "PROVISIONED"
+  hash_key     = "houses"
+  range_key    = "date"
+
+  attribute {
+    name = "houses"
+    type = "S"
+  }
+
+  attribute {
+    name = "date"
+    type = "S"
+  }
+
+  attribute {
+    name = "user"
+    type = "S"
+  }
+
+  attribute {
+    name = "type"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name      = "UserType"
+    hash_key  = "user"
+    range_key = "type"
+  }
+}
