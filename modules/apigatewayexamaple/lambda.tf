eb3f577707d7ab693ace540908377b84c6f7c9c7
sergio ortiz <audel91@gmail.com>
2023-06-21 12:23:04 -0600

    add role

--- modules/apigatewayexamaple/lambda.tf
+++ modules/apigatewayexamaple/lambda.tf
@@ -37,3 +37,24 @@ resource "aws_lambda_permission" "lambda" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.api_gateway_rest_api.execution_arn}/*/*"
 }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_lambda_apigw"
+
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+        }
+    ]
+    }
+    EOF
+
+}
