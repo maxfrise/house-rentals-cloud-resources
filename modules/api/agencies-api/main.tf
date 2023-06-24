5776f1822ec2f773bc3b3e3199d27671f7cd0e17
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-23 21:20:26 -0700

    refactor: comment for lambda

--- modules/api/agencies-api/main.tf
+++ modules/api/agencies-api/main.tf
@@ -1,38 +1,10 @@
-resource "aws_lambda_function" "agencies" {
-  filename         = "${var.building_path}/${var.lambda_code_filename}"
-  handler          = "index.handler"
-  runtime          = "nodejs18.x"
-  function_name    = "agencies"
-  architectures    = ["arm64"]
-  role             = aws_iam_role.iam_for_agencies_lambda.arn
-  timeout          = 30
-  source_code_hash = filebase64sha256("${var.building_path}/${var.lambda_code_filename}")
-}
-
-resource "aws_iam_role" "iam_for_agencies_lambda" {
-  name = "iam_for_agencies_lambda"
-
-  assume_role_policy = <<EOF
-    {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-        }
-    ]
-    }
-    EOF
-
-}
-
 resource "aws_api_gateway_rest_api" "agencies_api" {
   name        = "agencies_api"
   description = "Api Gateway for agencies management"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_resource" "agency_resource" {
@@ -100,4 +72,38 @@ resource "aws_api_gateway_stage" "prod" {
   deployment_id = aws_api_gateway_deployment.prod_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.agencies_api.id
   stage_name    = "prod"
+}
+
+### LAMBDA Function
+
+resource "aws_lambda_function" "agencies" {
+  filename         = "${var.building_path}/${var.lambda_code_filename}"
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  function_name    = "agencies"
+  architectures    = ["arm64"]
+  role             = aws_iam_role.iam_for_agencies_lambda.arn
+  timeout          = 30
+  source_code_hash = filebase64sha256("${var.building_path}/${var.lambda_code_filename}")
+}
+
+resource "aws_iam_role" "iam_for_agencies_lambda" {
+  name = "iam_for_agencies_lambda"
+
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+        }
+    ]
+    }
+    EOF
+
 }
