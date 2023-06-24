3d9a68fe718560f0013cb1835cf871cf66b5bcaa
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-23 18:57:28 -0700

    fix: resources

--- modules/api/agencies-api/main.tf
+++ modules/api/agencies-api/main.tf
@@ -1,25 +1,25 @@
-resource "aws_api_gateway_rest_api" "agencies_api_gateway" {
+resource "aws_api_gateway_rest_api" "agencies_api" {
   name        = "agencies_api"
   description = "Api Gateway for agencies management"
 }
 
-resource "aws_api_gateway_resource" "agencies_api" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway_rest_api.root_resource_id
+resource "aws_api_gateway_resource" "agency_resource" {
+  rest_api_id = aws_api_gateway_rest_api.agencies_api.id
+  parent_id   = aws_api_gateway_rest_api.agencies_api.root_resource_id
   path_part   = "agenciesapi"
 }
 
-resource "aws_api_gateway_method" "api_gateway_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway_rest_api.id
-  resource_id   = aws_api_gateway_resource.api_gateway.id
+resource "aws_api_gateway_method" "agency_resource_method" {
+  rest_api_id   = aws_api_gateway_rest_api.agencies_api.id
+  resource_id   = aws_api_gateway_resource.agency_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_gateway_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
-  resource_id = aws_api_gateway_method.api_gateway_method.resource_id
-  http_method = aws_api_gateway_method.api_gateway_method.http_method
+resource "aws_api_gateway_integration" "agencies_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.agencies_api.id
+  resource_id = aws_api_gateway_method.agency_resource_method.resource_id
+  http_method = aws_api_gateway_method.agency_resource_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -28,7 +28,7 @@ resource "aws_api_gateway_integration" "api_gateway_integration" {
 
 resource "aws_api_gateway_deployment" "test_deployment" {
   depends_on = [
-    aws_api_gateway_integration.api_gateway_integration
+    aws_api_gateway_integration.agencies_lambda_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
@@ -41,7 +41,7 @@ resource "aws_api_gateway_deployment" "test_deployment" {
 
 resource "aws_api_gateway_deployment" "prod_deployment" {
   depends_on = [
-    aws_api_gateway_integration.api_gateway_integration
+    aws_api_gateway_integration.agencies_lambda_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
