d950ecc99982f09f8b0d79d90b4f9090978f84d7
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-26 16:53:06 -0700

    refactor: using archive file for lambda example (#36)

--- modules/lambdaexample/variables.tf
+++ modules/lambdaexample/variables.tf
@@ -12,3 +12,8 @@ variable "lambda_src_path" {
   description = "The path of the source code of the lambda"
   type        = string
 }
+
+variable "lambda_src_file" {
+  description = "The src file of the lambda"
+  type        = string
+}
