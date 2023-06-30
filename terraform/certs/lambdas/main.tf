077edb0e3dc9eb1528be24beb23ee4fded426882
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-29 18:58:20 -0700

    fix: remove

--- terraform/certs/lambdas/main.tf
+++ terraform/certs/lambdas/main.tf
@@ -17,23 +17,10 @@ resource "aws_iam_role" "iam_maxfrise_lambdas" {
                 "Sid": "TouchDynamoTables",
                 "Effect": "Allow",
                 "Action": [
-                    "dynamodb:UpdateGlobalTable",
-                    "dynamodb:DeleteTable",
-                    "dynamodb:DescribeTable",
                     "dynamodb:GetItem",
-                    "dynamodb:DescribeExport",
-                    "dynamodb:BatchGetItem",
-                    "dynamodb:BatchWriteItem",
                     "dynamodb:PutItem",
-                    "dynamodb:Scan",
                     "dynamodb:UpdateItem",
-                    "dynamodb:CreateTable",
-                    "dynamodb:UpdateTable",
-                    "dynamodb:GetRecords",
-                    "dynamodb:DescribeImport",
                     "dynamodb:DeleteItem",
-                    "dynamodb:CreateBackup",
-                    "dynamodb:ConditionCheckItem",
                     "dynamodb:Query"
                 ],
                 "Resource": "arn:aws:dynamodb:*:914036813947:table/*"
