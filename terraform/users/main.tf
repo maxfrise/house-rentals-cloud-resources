4a7f2ee38f5e65e95089533547a332a00164219d
Felipe Nava <felipe.nava.co@gmail.com>
2023-07-04 14:13:20 -0700

    feat: adding maxfrise users pool

---
+++ terraform/users/main.tf
@@ -0,0 +1,28 @@
+resource "aws_cognito_user_pool" "maxfrise-users" {
+  name = "maxfrise_users"
+
+  mfa_configuration          = "ON"
+  sms_authentication_message = "Your code is {####}"
+
+  sms_configuration {
+    external_id    = "example"
+    sns_caller_arn = // ARN FOR SNS
+    sns_region     = "us-east-1"
+  }
+
+  software_token_mfa_configuration {
+    enabled = true
+  }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  }
+}
