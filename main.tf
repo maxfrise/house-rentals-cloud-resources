348ddadd577ab7ac0f8308e12464f24d9fe88b2e
sergio ortiz <audel91@gmail.com>
2023-07-09 14:01:30 -0700

    fixed

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ module "maxfrise_user_pool" {
       generate_secret                      = true
       logout_urls                          = ["https://www.maxfrise.com/logout"]
       name                                 = "maxfrise_web_client"
-      read_attributes                      = ["email"]
+      read_attributes                      = ["email", "profile"]
       supported_identity_providers         = []
       write_attributes                     = ["email", "name"]
       access_token_validity                = 1
