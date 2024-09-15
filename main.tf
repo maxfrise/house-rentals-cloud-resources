541d39672d4305252f3576b96bc811655b3065f5
Sergio Ortiz <seortiz@expediagroup.com>
2024-09-15 16:36:42 -0700

    format

--- main.tf
+++ main.tf
@@ -156,12 +156,12 @@ data "template_file" "maxfrise_api_v2_spec" {
   template = file("./api/api_spec.yaml")
 
   vars = {
-    region                      = "us-west--2"
-    initLeaseLambdaArn          = module.api_lambdas["initLease"].lambda_function_arn
-    paymentCollectorLambdaArn   = module.api_lambdas["paymentCollector"].lambda_function_arn
-    createHouseLambdaArn        = module.api_lambdas["createHouse"].lambda_function_arn
-    getHousesLambdaArn          = module.api_lambdas["getHouses"].lambda_function_arn
-    houseOverviewLambdaArn      = module.api_lambdas["houseOverview"].lambda_function_arn
+    region                    = "us-west--2"
+    initLeaseLambdaArn        = module.api_lambdas["initLease"].lambda_function_arn
+    paymentCollectorLambdaArn = module.api_lambdas["paymentCollector"].lambda_function_arn
+    createHouseLambdaArn      = module.api_lambdas["createHouse"].lambda_function_arn
+    getHousesLambdaArn        = module.api_lambdas["getHouses"].lambda_function_arn
+    houseOverviewLambdaArn    = module.api_lambdas["houseOverview"].lambda_function_arn
   }
 }
 
