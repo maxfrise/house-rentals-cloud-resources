e465ba6a845b7dd2f341e65d657ea6a12fdf843f
Sergio Ortiz <seortiz@expediagroup.com>
2024-10-02 14:42:26 -0700

    typos

--- api/domain.tf
+++ api/domain.tf
@@ -27,7 +27,7 @@ resource "aws_api_gateway_base_path_mapping" "staging_v2_domain" {
 #
 # Production
 #
-resource "aws_api_gateway_domain_name" "staging_v2_domain" {
+resource "aws_api_gateway_domain_name" "production_v2_domain" {
   domain_name              = "apiv2.maxfrise.com"
   regional_certificate_arn = data.aws_acm_certificate.maxfrise.arn
 
@@ -36,7 +36,7 @@ resource "aws_api_gateway_domain_name" "staging_v2_domain" {
   }
 }
 
-resource "aws_api_gateway_base_path_mapping" "staging_v2_domain" {
+resource "aws_api_gateway_base_path_mapping" "production_v2_domain" {
   api_id      = aws_api_gateway_rest_api.api_v2.id
   domain_name = aws_api_gateway_domain_name.staging_v2_domain.domain_name
   stage_name  = aws_api_gateway_stage.prod_stage.stage_name
