6616ac14f1d90894b0a4ea8a040b458a051b0777
Sergio Ortiz <seortiz@expediagroup.com>
2024-10-02 11:16:46 -0700

    remove self sigened domain

--- api/output.tf
+++ api/output.tf
@@ -2,14 +2,8 @@ output "api_gateway_execution_arn" {
   value = aws_api_gateway_rest_api.api_v2.execution_arn
 }
 
-output "curl_domain_url" {
-  depends_on = [aws_api_gateway_base_path_mapping.example]
-
-  description = "API Gateway Domain URL (self-signed certificate)"
-  value       = "curl -H 'Host: {domain}' https://${aws_api_gateway_domain_name.example.regional_domain_name}${var.rest_api_path} # may take a minute to become available on initial deploy"
-}
 
 output "curl_stage_invoke_url" {
   description = "API Gateway Stage Test Invoke URL"
-  value       = "curl ${aws_api_gateway_stage.example.invoke_url}/{endpoint}}"
+  value       = "curl ${aws_api_gateway_stage.test_stage.invoke_url}/{resource}}"
 }
