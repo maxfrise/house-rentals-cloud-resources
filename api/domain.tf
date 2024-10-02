7c79b3efb5233e049b9d35dbf9f32cf60d2b19c1
Sergio Ortiz <seortiz@expediagroup.com>
2024-10-02 14:37:04 -0700

    add domain to api

---
+++ api/domain.tf
@@ -0,0 +1,47 @@
+data "aws_acm_certificate" "maxfrise" {
+  domain = "*.maxfrise.com"
+
+  # Use the most recent certificate
+  most_recent = true
+
+  # Ensure the certificate is in the same region as your API Gateway domain
+  statuses = ["ISSUED"]
+}
+
+#
+# Staging
+#
+
+resource "aws_api_gateway_domain_name" "staging_v2_domain" {
+  domain_name              = "staging.apiv2.maxfrise.com"
+  regional_certificate_arn =  data.aws_acm_certificate.maxfrise.arn
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "staging_v2_domain" {
+  api_id      = aws_api_gateway_rest_api.api_v2.id
+  domain_name = aws_api_gateway_domain_name.staging_v2_domain.domain_name
+  stage_name  = aws_api_gateway_stage.test_stage.stage_name
+}
+
+#
+# Production
+#
+
+resource "aws_api_gateway_domain_name" "staging_v2_domain" {
+  domain_name              = "apiv2.maxfrise.com"
+  regional_certificate_arn = data.aws_acm_certificate.maxfrise.arn
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "staging_v2_domain" {
+  api_id      = aws_api_gateway_rest_api.api_v2.id
+  domain_name = aws_api_gateway_domain_name.staging_v2_domain.domain_name
+  stage_name  = aws_api_gateway_stage.prod_stage.stage_name
+}
