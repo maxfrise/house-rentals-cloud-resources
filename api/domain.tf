89a91e4f14976ab9a86cfa714a93e33dc3382c87
Sergio Ortiz <seortiz@expediagroup.com>
2024-10-02 16:30:47 -0700

    add staging

--- api/domain.tf
+++ api/domain.tf
@@ -6,6 +6,32 @@ data "aws_acm_certificate" "maxfrise" {
   statuses = ["ISSUED"]
 }
 
+data "aws_acm_certificate" "maxfrise_test" {
+  domain = "*.apiv2.maxfrise.com"
+
+  most_recent = true
+
+  statuses = ["ISSUED"]
+}
+
+#
+# Staging
+#
+resource "aws_api_gateway_domain_name" "staging_v2_domain" {
+  domain_name              = "staging.apiv2.maxfrise.com"
+  regional_certificate_arn =  data.aws_acm_certificate.maxfrise_test.arn
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "staging_v2_domain" {
+  api_id      = aws_api_gateway_rest_api.api_v2.id
+  domain_name = aws_api_gateway_domain_name.staging_v2_domain.domain_name
+  stage_name  = aws_api_gateway_stage.test_stage.stage_name
+}
+
 #
 # Production
 #
