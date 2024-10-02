a5d37096b49b5d586d7be846f6b7e5c4a609ed65
Sergio Audel Ortiz Gutierrez <audel91@gmail.com>
2024-10-02 15:08:28 -0700

    remove staging (#117)
    Co-authored-by: Sergio Ortiz <seortiz@expediagroup.com>

--- api/domain.tf
+++ api/domain.tf
@@ -7,24 +7,6 @@ data "aws_acm_certificate" "maxfrise" {
 }
 
 #
-# Staging
-#
-resource "aws_api_gateway_domain_name" "staging_v2_domain" {
-  domain_name              = "staging.apiv2.maxfrise.com"
-  regional_certificate_arn =  data.aws_acm_certificate.maxfrise.arn
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_base_path_mapping" "staging_v2_domain" {
-  api_id      = aws_api_gateway_rest_api.api_v2.id
-  domain_name = aws_api_gateway_domain_name.staging_v2_domain.domain_name
-  stage_name  = aws_api_gateway_stage.test_stage.stage_name
-}
-
-#
 # Production
 #
 resource "aws_api_gateway_domain_name" "production_v2_domain" {
@@ -38,6 +20,6 @@ resource "aws_api_gateway_domain_name" "production_v2_domain" {
 
 resource "aws_api_gateway_base_path_mapping" "production_v2_domain" {
   api_id      = aws_api_gateway_rest_api.api_v2.id
-  domain_name = aws_api_gateway_domain_name.staging_v2_domain.domain_name
+  domain_name = aws_api_gateway_domain_name.production_v2_domain.domain_name
   stage_name  = aws_api_gateway_stage.prod_stage.stage_name
 }
